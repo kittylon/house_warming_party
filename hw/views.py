7d5a1f882dfbd3ae1bf426aa4b1627cfdcecb70e
Carolina Londo A <carolin861@gmail.com>
2018-02-05 16:35:55 -0500

    Views and templates added"

--- hw/views.py
+++ hw/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def invitation_list(request):
+    return render(request, 'hw/invitation_list.html', {})
--- hwproject/urls.py
+++ hwproject/urls.py
@@ -11,11 +11,14 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('hw/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('hw.urls')),
 ]
