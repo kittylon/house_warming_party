1cecf81201f95004c9bab59c33938ca5c3027108
Carolina <carolin861@gmail.com>
2018-03-08 23:00:46 -0500

    Demo

--- hwproject/wsgi.py
+++ hwproject/wsgi.py
@@ -14,6 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hwproject.settings")
 
 application = get_wsgi_application()
-
-from hwproject.wsgi import hw
-application = hw(application)
